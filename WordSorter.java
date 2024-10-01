bb7c5a6d22be067ed249d65e445e855ccbf29072
NadiaEs829 <nadia_amores@hotmail.com>
2024-10-01 17:50:21 +0000

    working - end of 10/1

--- WordSorter.java
+++ WordSorter.java
@@ -5,15 +5,18 @@ public class WordSorter
 	public static void main(String[] args) 
 	{
 		Scanner in = new Scanner(System.in); 
+		//ArrayList <String> uniqueWords = new ArrayList <String>();
 		try
 		{
 			in = new Scanner(new File("article.txt"));	
+			Scanner input = new Scanner(System.in);
 		}
 		catch(Exception e)
 		{
 			System.out.println("Cannot find file... Exiting Program");
 			return;
 		}	
+
 		String word = "";
 		while(in.hasNext())
 		{
@@ -30,7 +33,18 @@ public class WordSorter
 			word = word.replace("'", "");
 			System.out.println(word);
 			
+
 			
-		}
-	}
-}
+		}//end while
+
+		System.out.println("Choose one of the options listed:");
+		System.out.println("1. Print out all the words starting with a specific letter.");
+		System.out.println("2. Print out all words");
+		System.out.println("3. Print out the total number of total unique words");
+		System.out.println("4. Search and determine if a word is in the article");
+		System.out.println("5. Remove a word from the data structure");
+		System.out.println("6. Exit");
+		int option = input.nextInt();
+
+	}//end main
+}//end class
