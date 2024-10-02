6669d84d21982984a20283b82afff3a6162dd584
NadiaEs829 <nadia_amores@hotmail.com>
2024-10-02 17:39:11 +0000

    working - end of 10/2

--- WordSorter.java
+++ WordSorter.java
@@ -1,15 +1,19 @@
 import java.io.File;
 import java.util.Scanner;
+import java.util.ArrayList;
+//Nadia Escamilla, 10/2, pd:7
+//process through all values and print words based on user input
 public class WordSorter 
 {
 	public static void main(String[] args) 
 	{
 		Scanner in = new Scanner(System.in); 
-		//ArrayList <String> uniqueWords = new ArrayList <String>();
+		Scanner input = new Scanner(System.in);
+		ArrayList <String> uniqueWords = new ArrayList <String>();
 		try
 		{
 			in = new Scanner(new File("article.txt"));	
-			Scanner input = new Scanner(System.in);
+			
 		}
 		catch(Exception e)
 		{
@@ -31,12 +35,10 @@ public class WordSorter
 			word = word.replace("!", "");
 			word = word.replace("?", "");
 			word = word.replace("'", "");
-			System.out.println(word);
-			
-
-			
+			//System.out.println(word);
+			uniqueWords.add(word);
 		}//end while
-
+		
 		System.out.println("Choose one of the options listed:");
 		System.out.println("1. Print out all the words starting with a specific letter.");
 		System.out.println("2. Print out all words");
@@ -45,6 +47,21 @@ public class WordSorter
 		System.out.println("5. Remove a word from the data structure");
 		System.out.println("6. Exit");
 		int option = input.nextInt();
+
+		if(option == 1){
+			System.out.println("Enter a letter");
+			char letter = input.nextLine();
+			int convert = (int)letter;
+
+			for(int i = 0; i <= uniqueWords.size(); i++){
+				if(uniqueWords(i) == convert)
+					System.out.println(word);
+			}//end for loop
+		}//end if
+
+		if(option == 2){
+			System.out.println(uniqueWords);
+		}//end if
 
 	}//end main
 }//end class
